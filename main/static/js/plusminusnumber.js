305c7b474a442bdf26ca4d2a18002de37d4f2c72
bukeme <basseyukemme@gmail.com>
2023-02-17 09:46:12 +0100

    push project to github

--- main/static/js/plusminusnumber.js
+++ main/static/js/plusminusnumber.js
@@ -2,14 +2,10 @@ $(function() {
 	$('.btn-number').click(function(e) {
 		e.preventDefault();
 		fieldName = $(this).attr('data-field');
-		console.log(fieldName, 'field');
 		type = $(this).attr('data-type');
 		var input = $('input[name="'+ fieldName + '"]');
 		var currentVal = +input.val();
-		console.log(input);
-		console.log(currentVal, 'val');
 		if (type == 'minus') {
-			console.log('minus');
 			if (currentVal > input.attr('min')) {
 				input.val(currentVal -1).change();
 			};
@@ -18,10 +14,8 @@ $(function() {
 			};
 		}
 		else if (type == 'plus') {
-			console.log('plus');
 			if (currentVal < input.attr('max')) {
 				input.val(currentVal + 1).change();
-				console.log(currentVal);
 			};
 			if (parseInt(input.val()) == input.attr('max')) {
 				$(this).attr('disabled', true);
